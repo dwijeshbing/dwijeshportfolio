b7c14fc01e0c82023f3717f76f8f515198db3757
dwijeshbing <dwijeshbingumalla@gmail.com>
2024-07-10 11:34:07 +0530

    changes made for handling home page

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+
+const homepage = process.env.REACT_APP_HOMEPAGE || '/';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode basename={homepage}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
