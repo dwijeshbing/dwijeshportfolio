363b931e2d62aefaf879da8d12ff57840d4afc63
dwijeshbing <dwijeshbingumalla@gmail.com>
2024-07-10 12:49:49 +0530

    Sphere fixed

--- src/components/Sphere/index.js
+++ src/components/Sphere/index.js
@@ -4,41 +4,40 @@ import TagCloud from "TagCloud";
 
 const Sphere = () => {
     useEffect(() => {
-        return () => {
-            const container = ".tagcloud";
-            const texts = [
-                "Python",
-                "Java",
-                "C",
-                "C++",
-                "HTML",
-                "CSS",
-                "SASS",
-                "ExpressJS",
-                "JavaScript",
-                "React",
-                "NodeJS",
-                "Jquery",
-                "GITHUB",
-                "MongoDB",
-                "MySql"
-            ];
-            const getRadius = () => {
-                const width = window.innerWidth;
-                if (width <= 480) return 100;
-                if (width <= 768) return 150;
-                if (width <= 1024) return 200;
-                return 300;
-            };
-
-            const options = {
-                radius: getRadius(),
-                maxSpeed: "fast",
-                initSpeed: "normal",
-            };
-
-            TagCloud(container, texts, options);
+        const container = '.tagcloud';
+        const texts = [
+            "Python",
+            "Java",
+            "C",
+            "C++",
+            "HTML",
+            "CSS",
+            "SASS",
+            "ExpressJS",
+            "JavaScript",
+            "React",
+            "NodeJS",
+            "Jquery",
+            "GITHUB",
+            "MongoDB",
+            "MySql"
+        ];
+        const getRadius = () => {
+            const width = window.innerWidth;
+            if (width <= 480) return 100;
+            else if (width <= 768) return 150;
+            else if (width <= 1024) return 200;
+            return 300;
         };
+
+        const options = {
+            radius: getRadius(),
+            maxSpeed: 'normal',
+            initSpeed: 'normal',
+            keep: true,
+        };
+
+        TagCloud(container, texts, options);
     }, []);
 
     return (
